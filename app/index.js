091209fffa2f99392534a1fddc8867693be30b35
Samuel Georges <sam@daftspunk.com>
2019-01-01 18:24:08 +1100

    Include deps

--- app/index.js
+++ app/index.js
@@ -3,26 +3,29 @@ import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import Root from './containers/Root';
 import { configureStore, history } from './store/configureStore';
-import './app.global.css';
+import handleWindowControls from './utils/bindTitlebar';
+import './app.global.scss';
 
 const store = configureStore();
 
 render(
-  <AppContainer>
-    <Root store={store} history={history} />
-  </AppContainer>,
-  document.getElementById('root')
+    <AppContainer>
+        <Root store={store} history={history} />
+    </AppContainer>,
+    document.getElementById('root')
 );
 
 if (module.hot) {
-  module.hot.accept('./containers/Root', () => {
-    // eslint-disable-next-line global-require
-    const NextRoot = require('./containers/Root').default;
-    render(
-      <AppContainer>
-        <NextRoot store={store} history={history} />
-      </AppContainer>,
-      document.getElementById('root')
-    );
-  });
+    module.hot.accept('./containers/Root', () => {
+        // eslint-disable-next-line global-require
+        const NextRoot = require('./containers/Root').default;
+        render(
+            <AppContainer>
+                <NextRoot store={store} history={history} />
+            </AppContainer>,
+            document.getElementById('root')
+        );
+    });
 }
+
+handleWindowControls();
